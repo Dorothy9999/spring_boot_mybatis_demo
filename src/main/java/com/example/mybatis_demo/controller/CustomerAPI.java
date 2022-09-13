953e73971e25a686d12d797e217ced3bd937ae73
Dorothy9999 <pfongking@gmail.com>
2022-09-13 22:09:58 +0800

    Initial commit

---
+++ src/main/java/com/example/mybatis_demo/controller/CustomerAPI.java
@@ -0,0 +1,7 @@
+package com.example.mybatis_demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerAPI {
+}
---
+++ src/main/java/com/example/mybatis_demo/mapper/CustomerMapper.java
@@ -0,0 +1,7 @@
+package com.example.mybatis_demo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CustomerMapper {
+}
